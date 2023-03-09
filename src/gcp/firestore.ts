5494467dfc3a333802cde37e6886404ac0149a7d
TBSten <81161390+TBSten@users.noreply.github.com>
2023-03-09 22:41:45 +0900

    不要な関数を削除

--- src/gcp/firestore.ts
+++ src/gcp/firestore.ts
@@ -1,7 +1,3 @@
 import { Firestore } from "@google-cloud/firestore";
 
 export const db = new Firestore()
-
-export const log = async () => {
-    await db.collection("log").add({ ts: new Date() })
-}
