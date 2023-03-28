066124f85326d2d71cd35a052a54bfef30c27289
TBSten <81161390+TBSten@users.noreply.github.com>
2023-03-29 01:41:31 +0900

    メニューなどを修正

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -1,25 +1,26 @@
 import { Box, BoxProps } from "@mui/material";
 import { FC, ReactNode } from "react";
 import BaseHeader from "./BaseHeader";
-import BaseMenu, { useTopMenu } from "./BaseMenu";
+import BaseMenu from "./BaseMenu";
 
 interface BaseLayoutProps {
     containView?: boolean
     children: ReactNode
     header?: ReactNode
+    isAdmin?: boolean
 }
 const BaseLayout: FC<BaseLayoutProps> = ({
     containView = false,
     children,
+    isAdmin = false,
 }) => {
-    const topMenu = useTopMenu()
     const boxProps: BoxProps = containView
         ? { height: "100%" }
         : { minHeight: "100%" }
     return (
         <Box width="100%" height="100%" {...boxProps} bgcolor={t => t.palette.grey[100]} overflow="auto">
-            <BaseMenu />
-            <BaseHeader />
+            <BaseMenu isAdmin={isAdmin} />
+            <BaseHeader isAdmin={isAdmin} />
             {children}
         </Box>
     );
